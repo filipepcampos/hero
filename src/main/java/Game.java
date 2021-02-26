963addbc4ce8189f7da46c653bb54bd93aaca4a6
Filipe Campos <filipepcampos24@gmail.com>
2021-02-26 14:41:06 +0000

    Our HERO has arrived

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -7,8 +9,12 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
+import static com.googlecode.lanterna.input.KeyType.*;
+
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
     Game(){
         try {
@@ -26,7 +32,13 @@ public class Game {
 
     public void run(){
         try {
-            this.draw();
+            while(true){
+                this.draw();
+                KeyStroke key = screen.readInput();
+                if(!processKey(key)){
+                    break;
+                }
+            }
         } catch (IOException e){
             e.printStackTrace();
         }
@@ -34,7 +46,23 @@ public class Game {
 
     private void draw() throws IOException{
         screen.clear();
-        screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
         screen.refresh();
+    }
+
+    private boolean processKey(KeyStroke key) throws IOException{
+        switch (key.getKeyType()){
+            case ArrowUp: y -= 1; break;
+            case ArrowDown: y += 1; break;
+            case ArrowLeft: x -= 1; break;
+            case ArrowRight: x += 1; break;
+            case Character:
+                if(key.getCharacter() == 'q'){
+                    screen.close();
+                }
+                break;
+            case EOF: return false;
+        }
+        return true;
     }
 }
