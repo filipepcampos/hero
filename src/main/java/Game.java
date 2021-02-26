1d99350deb7cc1097ea818d8b45ebde83e2bd931
Filipe Campos <filipepcampos24@gmail.com>
2021-02-26 15:40:23 +0000

    Use square font

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,7 +5,10 @@ import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.swing.AWTTerminalFontConfiguration;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 import static com.googlecode.lanterna.input.KeyType.*;
@@ -16,14 +19,27 @@ public class Game {
 
     Game(){
         try {
+            // ---- Copy pasta from Slack ----
+            // Load Font
+            File fontFile = new File("./resources/square.ttf");
+            Font font =  Font.createFont(Font.TRUETYPE_FONT, fontFile);
+            // Register Font
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+            //Configure Default Terminal Factory
+            Font loadedFont = font.deriveFont(Font.PLAIN, 25);
+            AWTTerminalFontConfiguration fontConfig = AWTTerminalFontConfiguration.newInstance(loadedFont);
             TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = terminalFactory.createTerminal();
+            DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            defaultTerminalFactory.setForceAWTOverSwing(true);
+            defaultTerminalFactory.setTerminalEmulatorFontConfiguration(fontConfig);
+            // ---- End of Copy pasta ----
+            Terminal terminal = defaultTerminalFactory.createTerminal();
             screen = new TerminalScreen(terminal);
             screen.setCursorPosition(null);
             screen.startScreen();
             screen.doResizeIfNecessary();
-        } catch (IOException e){
+        } catch (IOException | FontFormatException e){
             e.printStackTrace();
         }
         arena = new Arena(40, 20);
